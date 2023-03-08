123457fcf9e15ec6637a19f466409ffbb6299639
Teymur <43016690+teymureyvaz@users.noreply.github.com>
2023-03-08 21:38:36 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('https').Server(app);
 const io = require('socket.io')(http);
-const port = 3000;
+const port = 8080;
 
 
 function onConnection(socket){
