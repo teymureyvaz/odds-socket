263ba0612fcb804e0eada0419e12fa895c081c4f
Teymur <43016690+teymureyvaz@users.noreply.github.com>
2023-03-08 13:20:25 +0400

    yeep

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 
 function onConnection(socket){
-  socket.on('odds', (msg) => {
+  socket.on('message', (msg) => {
     io.emit('chat message', msg);
   });
 }
