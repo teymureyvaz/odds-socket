dac6b64a0fefe1ccde7a015ddd45db55f45365e1
Teymur <43016690+teymureyvaz@users.noreply.github.com>
2023-03-10 00:01:35 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-'use strict';
-
 const express = require('express');
-const socketIO = require('socket.io');
+const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
 
 const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server);
+const wss = new Server({ server });
 
-io.on('connection', (socket) => {
+wss.on('connection', (ws) => {
   console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  ws.on('close', () => console.log('Client disconnected'));
 });
 
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 10000);
