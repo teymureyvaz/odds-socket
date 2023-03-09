9f91c5e39cc7c5def9c1339361cdd2d9199f6dd5
Teymur <43016690+teymureyvaz@users.noreply.github.com>
2023-03-10 00:06:34 +0400

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ const wss = new Server({ server });
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
+  ws.on('message', (data) => {
+    wss.clients.forEach((client) => {
+        client.send(data);
+    });
+    }) 
 });
-
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 10000);
