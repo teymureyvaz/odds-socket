1649dfa1b97c4db192415cfd96bf15c4afd8534b
Teymur <43016690+teymureyvaz@users.noreply.github.com>
2023-03-11 22:00:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-const http = require("http").createServer();
-const io = require("socket.io")(http);
-//Listen for a client connection 
-io.on("connection", (socket) => {
-    console.log(socket.client.conn.server.clientsCount)
-    //Socket is a Link to the Client 
-    console.log("New Client is Connected!");
-    //Here the client is connected and we can exchanged 
+// Importing the required modules
+const WebSocketServer = require('ws');
+ 
+// Creating a new websocket server
+const wss = new WebSocketServer.Server({ port: 8080 })
+ 
+// Creating connection using websocket
+wss.on("connection", ws => {
+    console.log("new client connected");
+ 
+    // sending message to client
+    ws.send('Welcome, you are connected!');
+ 
+    //on message from client
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`)
+    });
+ 
+    // handling what to do when clients disconnects from server
+    ws.on("close", () => {
+        console.log("the client has connected");
+    });
+    // handling client connection error
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
 });
-
-//Listen the HTTP Server 
-http.listen(port, () => {
-    console.log("Server Is Running Port: " + port);
-});
+console.log("The WebSocket server is running on port 8080");
