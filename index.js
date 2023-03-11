5968ab235c3885f073dc2a156b3fa3b63822c17b
Teymur <43016690+teymureyvaz@users.noreply.github.com>
2023-03-11 16:30:24 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const express = require('express');
-const { Server } = require('ws');
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ port: 3000 }); // ws://localhost:3000
 
-const PORT = process.env.PORT || 3000;
+// Static test var
+var test_message = {
+    'test': 'Response',
+    'test2': 'Response2'
+};
 
-const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    console.log('IT IS GETTING INSIDE CLIENTS');
+    console.log(client);
 
-const wss = new Server({ server });
+    // The data is coming in correctly
+    console.log(data);
+    client.send(data);
+  });
+};
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-  ws.on('message', (data) => {
-    
-    console.log(data.toString())
-    console.log(wss.clients.size)
-    wss.clients.forEach((client) => {
-        client.send(data);
-    });
-    }) 
+wss.on('connection', function(ws) {
+   ws.on('message', function(message) {
+     wss.broadcast(test_message);
+     console.log('Received: ' + message);
+   });
+
+   // TODO This is static just to check that the connection is properly working
+   ws.send('You successfully connected to the websocket.');
 });
