83c4be51af8a264161fb804dde0672d2a682bceb
Teymur <43016690+teymureyvaz@users.noreply.github.com>
2023-03-13 22:26:04 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-// Importing the required modules
-const WebSocketServer = require('ws');
- 
-// Creating a new websocket server
-const wss = new WebSocketServer.Server({ port: 8080 })
- 
-// Creating connection using websocket
-wss.on("connection", ws => {
- 
-    ws.on("message", data => {
-         odds = JSON.parse(data);
-         if(odds.hasOwnProperty('code')){
-            if (odds.code == "456789kjbvGVHUJ") {
-                 wss.clients.forEach(function each(client){
-                 console.log("sent to client")
-                 client.send(data.toString());
-               });
-            } 
-         }
-     
-    });
-    // handling what to do when clients disconnects from server
-    ws.on("close", () => {
-        console.log("the client has connected");
-    });
-    ws.onerror = function () {
-        console.log("Some Error occurred")
-    }
-});
-console.log("The WebSocket server is running on port 8080");
+const express = require('express');
+const app = express();
+const http = require('https').Server(app);
+const io = require('socket.io')(http);
+const port = 8080;
+
+
+function onConnection(socket){
+  socket.on('message', (msg) => {
+   io.broadcast(msg)
+  });
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
