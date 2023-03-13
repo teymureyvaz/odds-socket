f04ea880f65451076ccafe3eb214e787259efdc1
Teymur <43016690+teymureyvaz@users.noreply.github.com>
2023-03-13 11:12:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,22 +6,15 @@ const wss = new WebSocketServer.Server({ port: 8080 })
  
 // Creating connection using websocket
 wss.on("connection", ws => {
-    console.log("new client connected");
-    console.log(wss.clients.size);
  
-    // sending message to client
-    ws.send('Welcome, you are connected!');
- 
-    //on message from client
     ws.on("message", data => {
-        console.log(`Client has sent us: ${data}`)
+            console.log(data)
+            ws.send('Welcome, you are connected!');
     });
-  
     // handling what to do when clients disconnects from server
     ws.on("close", () => {
         console.log("the client has connected");
     });
-    // handling client connection error
     ws.onerror = function () {
         console.log("Some Error occurred")
     }
